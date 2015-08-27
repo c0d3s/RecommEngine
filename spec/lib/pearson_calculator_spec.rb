d5fd2ee776d88189e03b77f1c88f947e9b61874a
Cody Knauer <codyknauer@gmail.com>
2015-08-27 02:09:03 -0500

    add pearson spec

---
+++ spec/lib/pearson_calculator_spec.rb
@@ -0,0 +1,31 @@
+require 'calculator'
+require 'pearson_calculator'
+
+describe '.PearsonCalculator' do
+  let(:data) {
+    {'Ghostface Killah'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.5,
+ '27 Dresses'=> 3.0, 'The Avengers'=> 3.5, 'Pootie Tang'=> 2.5,
+ 'Titanic'=> 3.0},
+'RZA'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 3.5,
+ '27 Dresses'=> 1.5, 'The Avengers'=> 5.0, 'Titanic'=> 3.0,
+ 'Pootie Tang'=> 3.5},
+'Raekwon'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.0,
+ 'The Avengers'=> 3.5, 'Titanic'=> 4.0},
+'GZA'=> {'Snakes on a Plane'=> 3.5, '27 Dresses'=> 3.0,
+ 'Titanic'=> 4.5, 'The Avengers'=> 4.0,
+ 'Pootie Tang'=> 2.5},
+'Method Man'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0,
+ '27 Dresses'=> 2.0, 'The Avengers'=> 3.0, 'Titanic'=> 3.0,
+ 'Pootie Tang'=> 2.0},
+'ODB' => {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0,
+ 'Titanic'=> 3.0, 'The Avengers'=> 5.0, 'Pootie Tang'=> 3.5},
+'Inspectah Deck'=> {'Snakes on a Plane'=>4.5, 'Pootie Tang'=>1.0, 'The Avengers'=>4.0}}
+}
+  let(:pearson_calc) { Suadeo::PearsonCalculator.new(data: data, p1: 'ODB', p2: 'Raekwon') }
+
+  describe '#calc' do
+    it 'calculates pearson similarity' do
+      expect(pearson_calc.calc).to eq(0.13483997249264842)
+    end
+  end
+end
