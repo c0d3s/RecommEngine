4b4fdfb6aa8f8a59c15966e14b83d327378ac54b
Cody Knauer <codyknauer@gmail.com>
2015-08-27 02:05:55 -0500

    rename constants and instance vars in recommender

--- lib/recommender.rb
+++ lib/recommender.rb
@@ -1,15 +1,31 @@
 module Suadeo
   class Recommender
-    def initialize(data:, subject:, similarity:)
+    attr_reader :data, :subject, :similarity, :similarity_scores, :totals, :similarity_sums
+
+    DEFAULT_ALGORITHM = 'Pearson'
+
+    def initialize(data:, subject:, similarity: DEFAULT_ALGORITHM)
       @data = data
       @subject = subject
       @similarity = similarity
       @similarity_scores = {}
       @totals = {}
-      @sim_sums = {}
+      @similarity_sums = {}
       @totals.default = 0
-      @sim_sums.default = 0
+      @similarity_sums.default = 0
     end
+
+    def recs
+      @data.each_key do |comperate|
+        next if is_subject?(comperate) || non_positive_similarity?(comperate)
+        @data[comperate].each_key do |product|
+          update_sums(comperate, product) unless scored_by_subject?(@subject, product)
+        end
+      end
+      rankings
+    end
+
+    private
 
     def is_subject?(comperate)
       comperate == @subject
@@ -36,22 +52,12 @@ module Suadeo
 
     def update_sums(comperate, product)
       @totals[product] += @data[comperate][product] * score(comperate)
-      @sim_sums[product] += score(comperate)
-    end
-
-    def recs
-      @data.each_key do |comperate|
-        next if is_subject?(comperate) || non_positive_similarity?(comperate)
-        @data[comperate].each_key do |product|
-          update_sums(comperate, product) unless scored_by_subject?(@subject, product)
-        end
-      end
-      rankings
+      @similarity_sums[product] += score(comperate)
     end
 
     def rankings
       rankings = {}
-      @totals.each { |subject, total| rankings[subject] = total / @sim_sums[subject] }
+      @totals.each { |subject, total| rankings[subject] = total / @similarity_sums[subject] }
       rankings.sort_by{|k, v| v}.reverse
     end
   end
