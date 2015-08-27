1161bf19bd110d8cab9b5d4e26478ab3e7d4b057
Cody Knauer <codyknauer@gmail.com>
2015-08-27 13:47:06 -0500

    dry up specs with shared context

--- spec/lib/matcher_spec.rb
+++ spec/lib/matcher_spec.rb
@@ -1,29 +1,5 @@
-require 'matcher'
-require 'calculator'
-require 'euclidean_calculator'
-require 'pearson_calculator'
-require 'pry'
-
 describe '.matcher' do
-  let(:data) {
-    {'Ghostface Killah'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.5,
- '27 Dresses'=> 3.0, 'The Avengers'=> 3.5, 'Pootie Tang'=> 2.5,
- 'Titanic'=> 3.0},
-'RZA'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 3.5,
- '27 Dresses'=> 1.5, 'The Avengers'=> 5.0, 'Titanic'=> 3.0,
- 'Pootie Tang'=> 3.5},
-'Raekwon'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.0,
- 'The Avengers'=> 3.5, 'Titanic'=> 4.0},
-'GZA'=> {'Snakes on a Plane'=> 3.5, '27 Dresses'=> 3.0,
- 'Titanic'=> 4.5, 'The Avengers'=> 4.0,
- 'Pootie Tang'=> 2.5},
-'Method Man'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0,
- '27 Dresses'=> 2.0, 'The Avengers'=> 3.0, 'Titanic'=> 3.0,
- 'Pootie Tang'=> 2.0},
-'ODB' => {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0,
- 'Titanic'=> 3.0, 'The Avengers'=> 5.0, 'Pootie Tang'=> 3.5},
-'Inspectah Deck'=> {'Snakes on a Plane'=>4.5, 'Pootie Tang'=>1.0, 'The Avengers'=>4.0}}
-}
+  include_context "test data"
   let(:default_matcher) { Suadeo::Matcher.new(data: data, subject: 'ODB') }
   let(:euclidean_matcher) { Suadeo::Matcher.new(data: data, subject: 'ODB', similarity: 'Euclidean') }
   let(:default_2_matcher) { Suadeo::Matcher.new(data: data, subject: 'ODB', num: 2) }
--- spec/lib/recommender_spec.rb
+++ spec/lib/recommender_spec.rb
@@ -1,30 +1,5 @@
-require 'recommender'
-require 'calculator'
-require 'euclidean_calculator'
-require 'pearson_calculator'
-require 'pry'
-
 describe '.recommender' do
-  let(:data) {
-    {'Ghostface Killah'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.5,
- '27 Dresses'=> 3.0, 'The Avengers'=> 3.5, 'Pootie Tang'=> 2.5,
- 'Titanic'=> 3.0},
-'RZA'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 3.5,
- '27 Dresses'=> 1.5, 'The Avengers'=> 5.0, 'Titanic'=> 3.0,
- 'Pootie Tang'=> 3.5},
-'Raekwon'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.0,
- 'The Avengers'=> 3.5, 'Titanic'=> 4.0},
-'GZA'=> {'Snakes on a Plane'=> 3.5, '27 Dresses'=> 3.0,
- 'Titanic'=> 4.5, 'The Avengers'=> 4.0,
- 'Pootie Tang'=> 2.5},
-'Method Man'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0,
- '27 Dresses'=> 2.0, 'The Avengers'=> 3.0, 'Titanic'=> 3.0,
- 'Pootie Tang'=> 2.0},
-'ODB' => {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0,
- 'Titanic'=> 3.0, 'The Avengers'=> 5.0, 'Pootie Tang'=> 3.5},
-'Inspectah Deck'=> {'Snakes on a Plane'=>4.5, 'Pootie Tang'=>1.0, 'The Avengers'=>4.0}}
-}
-
+  include_context "test data"
   let(:default_recommender) { Suadeo::Recommender.new(data: data, subject: 'Raekwon') }
   let(:euclidean_recommender) { Suadeo::Recommender.new(data: data, subject: 'Raekwon', similarity: 'Euclidean') }
 
