167c13e6111a47b63f6fe2b99b71e3214afd5632
Cody Knauer <codyknauer@gmail.com>
2015-08-27 02:05:07 -0500

    refactor matcher

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -1,28 +1,46 @@
 module Suadeo
   class Matcher
-    def initialize(data:, subject:, similarity:, num:)
-      @data = :data
-      @subject = :subject
-      @similarity = :similarity
-      @num = :num
+    attr_reader :data, :subject, :similarity, :num
+
+    DEFUALT_ALGORITHM = 'Pearson'
+    DEFUALT_NUMBER = 3
+
+    def initialize(data:, subject:, similarity: DEFUALT_ALGORITHM, num: DEFUALT_NUMBER)
+      @data = data
+      @subject = subject
+      @similarity = similarity
+      @num = num
     end
 
     def top_matches
-      bottom_matches.reverse
+      all_matches.reverse[0..upper_limit]
     end
 
     def bottom_matches
-      scores.sort_by{|k, v| v}.reverse
+      all_matches[0..upper_limit]
     end
 
     private
+
+    def all_matches
+      scores.sort_by{|k, v| v}
+    end
 
     def scores
       scores = {}
       data.each_key do |k|
         next if subject == k
-        scores[k] = Module.const_get("Suadeo::Calc").send("#{@similarity}").send('new', data: @data, p1: @subject, p2: @k).send('calc')
+        scores[k] = similarity_calculator.new(data: @data, p1: @subject, p2: k).calc
       end
+      scores
+    end
+
+    def similarity_calculator
+      Module.const_get("Suadeo::#{@similarity}Calculator")
+    end
+
+    def upper_limit
+      num - 1
     end
   end
 end
