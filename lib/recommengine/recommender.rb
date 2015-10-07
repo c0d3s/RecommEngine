75c0c8151774adf821361f6180ee209cfa05a249
Cody Knauer <codyknauer@gmail.com>
2015-10-07 02:35:31 -0500

    refactor recommender

--- lib/recommengine/recommender.rb
+++ lib/recommengine/recommender.rb
@@ -1,12 +1,12 @@
 module RecommEngine
   class Recommender
-    attr_reader :data, :subject, :similarity_algorithm, :user_similarity_scores, :sum_of_weighted_scores_by_item, :sum_of_user_similarity_scores_by_item, :rankings
+    attr_reader :data, :subject, :similarity_algorithm, :user_similarity_scores, :sum_of_weighted_scores_by_item, :sum_of_user_similarity_scores_by_item, :predicted_scores
 
     def initialize(data:, subject:, similarity: RecommEngine::DEFAULT_ALGORITHM)
       @data = data
       @subject = subject
       @similarity_algorithm = similarity
-      @scores = {}
+      @predicted_scores = {}
       @user_similarity_scores = {}
       @sum_of_weighted_scores_by_item = {}
       @sum_of_user_similarity_scores_by_item = {}
@@ -16,60 +16,63 @@ module RecommEngine
 
     def recs
       calculate_weighted_totals
+      calculate_predicted_scores
       predicted_scores.sort_by{ |k, v| v }.reverse
     end
 
     def top_rec
       calculate_weighted_totals
+      calculate_predicted_scores
       predicted_scores.max_by{ |k, v| v }
     end
 
     private
 
     def calculate_weighted_totals
-      comperates.each do |comperate|
-        @data[comperate].each_key{ |item| update_cumulative_totals(comperate, item) unless scored_by_subject?(item) }
+      comparates.each do |comparate|
+        data[comparate].each_key{ |item| update_cumulative_totals(comparate, item) unless scored_by_subject?(item) }
       end
     end
 
-    def score(comperate)
-      return @user_similarity_scores[comperate] if @user_similarity_scores[comperate]
-      @user_similarity_scores[comperate] = similarity_calculator.new(data: @data, subject: @subject, comparate: comperate).calc
+    def calculate_predicted_scores
+      sum_of_weighted_scores_by_item.each { |item, sum_of_scores| predicted_scores[item] = average_weighted_similarity_score(item, sum_of_scores) }
+    end
+
+    def similarity_score(comparate)
+      user_similarity_scores[comparate] ||= similarity_calculator.new(data: calculator_data(comparate), subject: subject, comparate: comparate).calc
+    end
+
+    def calculator_data(comparate)
+      data.select{ |k, v| k == subject || k == comparate }
     end
 
     def similarity_calculator
-      Module.const_get("RecommEngine::#{@similarity_algorithm}Calculator")
+      Module.const_get("RecommEngine::#{similarity_algorithm}Calculator")
     end
 
-    def non_positive_similarity?(comperate)
-      score(comperate) <= 0
+    def non_positive_similarity?(comparate)
+      similarity_score(comparate) <= 0
     end
 
     def scored_by_subject?(item)
-      return unless @data[@subject][item]
-      !@data[@subject][item].zero? || @data[@subject].include?(item)
+      data[subject][item] && !data[subject][item].zero?
     end
 
-    def update_cumulative_totals(comperate, item)
-      @sum_of_weighted_scores_by_item[item] += weighted_score(comperate, item)
-      @sum_of_user_similarity_scores_by_item[item] += score(comperate)
+    def update_cumulative_totals(comparate, item)
+      sum_of_weighted_scores_by_item[item] += weighted_similarity_score(comparate, item)
+      sum_of_user_similarity_scores_by_item[item] += similarity_score(comparate)
     end
 
-    def weighted_score(comperate, item)
-      @data[comperate][item] * score(comperate)
+    def weighted_similarity_score(comparate, item)
+      data[comparate][item] * similarity_score(comparate)
     end
 
-    def average_weighted_score(item, sum_of_scores)
-      sum_of_scores / @sum_of_user_similarity_scores_by_item[item]
+    def average_weighted_similarity_score(item, sum_of_scores)
+      sum_of_scores / sum_of_user_similarity_scores_by_item[item]
     end
 
-    def predicted_scores
-      @sum_of_weighted_scores_by_item.each { |item, sum_of_scores| @scores[item] = average_weighted_score(item, sum_of_scores) }
-      @scores
-    end
-
-   def comperates
-     @data.dup.delete_if{ |k,v| k == @subject || non_positive_similarity?(k) }.keys
+   def comparates
+     data.dup.delete_if{ |k,v| k == subject || non_positive_similarity?(k) }.keys
    end
   end
 end
