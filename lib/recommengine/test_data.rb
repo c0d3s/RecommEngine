11de52561147c06d367178f3b1172248ea1e6f26
Cody Knauer <codyknauer@gmail.com>
2015-10-07 16:10:31 -0500

    move test data

---
+++ lib/recommengine/test_data.rb
@@ -0,0 +1,23 @@
+module RecommEngine
+  WU_TANG = {'Ghostface Killah'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.5, '27 Dresses'=> 3.0, 'The Avengers'=> 3.5, 'Pootie Tang'=> 2.5, 'Titanic'=> 3.0},
+  'RZA'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 3.5, '27 Dresses'=> 1.5, 'The Avengers'=> 5.0, 'Titanic'=> 3.0, 'Pootie Tang'=> 3.5},
+  'Raekwon'=> {'The Sixth Sense'=> 2.5, 'Snakes on a Plane'=> 3.0, 'The Avengers'=> 3.5, 'Titanic'=> 4.0},
+  'GZA'=> {'Snakes on a Plane'=> 3.5, '27 Dresses'=> 3.0, 'Titanic'=> 4.5, 'The Avengers'=> 4.0, 'Pootie Tang'=> 2.5},
+  'Method Man'=> {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0, '27 Dresses'=> 2.0, 'The Avengers'=> 3.0, 'Titanic'=> 3.0,'Pootie Tang'=> 2.0},
+  'ODB' => {'The Sixth Sense'=> 3.0, 'Snakes on a Plane'=> 4.0, 'Titanic'=> 3.0, 'The Avengers'=> 5.0, 'Pootie Tang'=> 3.5},
+  'Inspectah Deck'=> {'Snakes on a Plane'=>4.5, 'Pootie Tang'=>1.0, 'The Avengers'=>4.0}}
+
+  CRITICS = {"Lisa Rose"=>{"Lady in the Water"=>2.5, "Snakes on a Plane"=>3.5, "Just My Luck"=>3.0, "Superman Returns"=>3.5, "You, Me and Dupree"=>2.5, "The Night Listener"=>3.0},
+  "Gene Seymour"=>{"Lady in the Water"=>3.0, "Snakes on a Plane"=>3.5, "Just My Luck"=>1.5, "Superman Returns"=>5.0, "The Night Listener"=>3.0, "You, Me and Dupree"=>3.5},
+  "Michael Phillips"=>{"Lady in the Water"=>2.5, "Snakes on a Plane"=>3.0, "Superman Returns"=>3.5, "The Night Listener"=>4.0},
+  "Claudia Puig"=>{"Snakes on a Plane"=>3.5, "Just My Luck"=>3.0, "The Night Listener"=>4.5, "Superman Returns"=>4.0, "You, Me and Dupree"=>2.5},
+  "Mick LaSalle"=>{"Lady in the Water"=>3.0, "Snakes on a Plane"=>4.0, "Just My Luck"=>2.0, "Superman Returns"=>3.0, "The Night Listener"=>3.0, "You, Me and Dupree"=>2.0},
+  "Jack Matthews"=>{"Lady in the Water"=>3.0, "Snakes on a Plane"=>4.0, "The Night Listener"=>3.0, "Superman Returns"=>5.0, "You, Me and Dupree"=>3.5},
+  "Toby"=>{"Snakes on a Plane"=>4.5, "You, Me and Dupree"=>1.0,"Superman Returns"=>4.0}}
+
+  BOOKS = {alice: {"War and Peace" => 2.5, "Crime and Punishment" => 3.5},
+  bob: {"War of the Worlds" => 5.0, "War and Peace" => 1.5, "The Great Gatsby" => 4.0},
+  cindy: {"War and Peace" => 5.0, "The Great Gatsby" => 4.5, "War of the Worlds" => 3.0, "Twenty Thousand Leagues Under the Sea" => 3.0},
+  don: {"War of the Worlds" => 4.0, "The Great Gatsby" => 2.5, "Twenty Thousand Leagues Under the Sea" => 5.0, "Crime and Punishment" => 4.5, "War and Peace" => 3.0},
+  erica: {"War of the Worlds" => 3.0, "The Great Gatsby" => 4.5, "Twenty Thousand Leagues Under the Sea" => 4.0, "Crime and Punishment" => 4.5, "War and Peace" => 3.5}}
+end
