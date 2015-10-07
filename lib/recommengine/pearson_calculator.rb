f1a38de33cc6267c7dad6a17357cb975b13b8287
Cody Knauer <codyknauer@gmail.com>
2015-10-07 02:35:02 -0500

    refactor pearson

--- lib/recommengine/pearson_calculator.rb
+++ lib/recommengine/pearson_calculator.rb
@@ -1,40 +1,40 @@
 module RecommEngine
   class PearsonCalculator < Calculator
-    attr_accessor :sum_of_subject_ratings, :sum_of_comparate_ratings, :sum_of_sq_subject_ratings, :sum_of_sq_comparate_ratings, :sum_of_ratings_product
+    attr_reader :sum_of_subject_scores, :sum_of_comparate_scores, :sum_of_sq_subject_scores, :sum_of_sq_comparate_scores, :sum_of_scores_product, :_number_of_hits, :_similar_items
 
     def initialize(*args)
       super
-      @sum_of_subject_ratings = @sum_of_comparate_ratings = @sum_of_sq_subject_ratings = @sum_of_sq_comparate_ratings = @sum_of_ratings_product = 0
+      @sum_of_subject_scores = @sum_of_comparate_scores = @sum_of_sq_subject_scores = @sum_of_sq_comparate_scores = @sum_of_scores_product = 0
     end
 
     def calc
       return 0 if number_of_hits < 2
-      sum_all_similarities
+      sum_all_scores
       perform_equation
     end
 
     private
 
     def similar_items
-      @similar_items ||= @data[@subject].map{ |k, v| k if @data[@comparate].keys.include?(k) }.compact
+      @_similar_items ||= data[subject].map{ |k, v| k if data[comparate].keys.include?(k) }.compact
     end
 
     def number_of_hits
-      @number_of_hits ||= similar_items.length
+      @_number_of_hits ||= similar_items.length
     end
 
-    def sum_all_similarities
-      similar_items.each{ |item| sum_simalarities(item) }
+    def sum_all_scores
+      similar_items.each{ |item| sum_scores_for(item) }
     end
 
-    def sum_simalarities(item)
-      subject_val = @data[@subject][item]
-      comparate_val = @data[@comparate][item]
-      @sum_of_subject_ratings += subject_val
-      @sum_of_comparate_ratings += comparate_val
-      @sum_of_sq_subject_ratings += subject_val**2.0
-      @sum_of_sq_comparate_ratings += comparate_val**2.0
-      @sum_of_ratings_product += subject_val * comparate_val
+    def sum_scores_for(item)
+      subject_val = data[subject][item]
+      comparate_val = data[comparate][item]
+      @sum_of_subject_scores += subject_val
+      @sum_of_comparate_scores += comparate_val
+      @sum_of_sq_subject_scores += subject_val**2.0
+      @sum_of_sq_comparate_scores += comparate_val**2.0
+      @sum_of_scores_product += subject_val * comparate_val
     end
 
     def perform_equation
@@ -42,11 +42,11 @@ module RecommEngine
     end
 
     def numerator
-      @sum_of_ratings_product - (@sum_of_subject_ratings * @sum_of_comparate_ratings/number_of_hits)
+      sum_of_scores_product - (sum_of_subject_scores * sum_of_comparate_scores/number_of_hits)
     end
 
     def denominator
-      @denominator ||= Math.sqrt((@sum_of_sq_subject_ratings - (@sum_of_subject_ratings**2.0)/number_of_hits)*(@sum_of_sq_comparate_ratings - (@sum_of_comparate_ratings**2.0)/number_of_hits))
+      denominator ||= Math.sqrt((sum_of_sq_subject_scores - (sum_of_subject_scores**2.0)/number_of_hits)*(sum_of_sq_comparate_scores - (sum_of_comparate_scores**2.0)/number_of_hits))
     end
   end
 end
